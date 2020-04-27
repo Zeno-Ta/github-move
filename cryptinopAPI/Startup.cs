4d7db4f0170faeae94b662f5368c8d47e553217a
Zeno-Ta <tanekengt@yahoo.fr>
2020-04-26 21:29:04 -0400

    Igniore les fichiers de config_def

--- cryptinopAPI/Startup.cs
+++ cryptinopAPI/Startup.cs
@@ -1,17 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using cryptinopAPI.DAL.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace cryptinopAPI
 {
@@ -24,7 +17,6 @@ namespace cryptinopAPI
                 .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
                 .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true)
                 .AddEnvironmentVariables();
-
 
             Configuration = builder.Build();
           
